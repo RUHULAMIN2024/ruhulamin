643eb65b368deb8943eb05a13df4235f1c667c94
RUHULAMIN2024 <ruhulamin15jun1999@gmail.com>
2024-07-11 18:10:08 +0600

    About section  added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -68,7 +68,7 @@ const Contact = () => {
             <SiGmail className="icons" />
           </a>
         </div>
-        <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-100 dark:text-gray-800">
+        <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32">
           <div className="flex flex-col justify-between">
             <div className="space-y-2">
               <h2 className="text-4xl font-bold leading-tight lg:text-4xl">Let`s talk!</h2>
